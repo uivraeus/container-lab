8284539437df0162862e328c571f573b1ad56495
Ulf Ivraeus <ulf.ivraeus@gmail.com>
2023-04-30 12:54:08 +0200

    Ubuntu 22.04 as base and better podman install

---
+++ extra-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# podman
+apt update && apt install -y podman
+
+# helm
+curl -sSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# kubectl
+curl -sSL "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl && chmod +x /usr/local/bin/kubectl
+
+# yq
+curl -sSL https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/local/bin/yq && chmod +x /usr/local/bin/yq
